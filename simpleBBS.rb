30b0b3998ffd83235e5ec89436d20519031f95fb
earth429 <matsu8dai20@gmail.com>
2020-10-27 10:20:41 +0900

    完成(仮)

--- simpleBBS.rb
+++ simpleBBS.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'active_record'
+require "cgi/escape"
 require 'pp'
 
 set :environment, :production
@@ -14,28 +15,52 @@ end
 get '/'do
     @s = Write.all
     erb :index
-end
+end;
 
 post '/new' do
+    # 時間の取得
     time = Time.now
+    # 書き込まれた書き込みの一つ前の書き込みのIDを取得
     id_count = Write.select('id').last
     if id_count == nil
-        id_count = 0
+        id_count = 1
     else
         id_count.id += 1
     end
-    pp params[:name]
+
+    # 書き込まれた名前とメッセージを変数に格納
+    name = params[:name]
+    message = params[:message]
+
+    # サニタイジング
+    name = CGI.escapeHTML(name)
+    message = CGI.escapeHTML(message)
+
+    # fontのみ許可
+    message = CGI.unescapeElement(message, "font")
+
+    # 改行の対応
+    message = message.gsub(/(\r\n|\n|\r)/, '<br>')
+
+    pp name
+    pp message
     
     s = Write.new
     s.id = id_count.id
-    if params[:name] == ""
-        s.name = "名無しのJ科生"
+    if name == ""
+        s.name = "名無しのJ科生" # もしも名前が書き込まれなかったら
     else
-        s.name = params[:name]
+        s.name = name
     end
-    pp s.name
+
+    if message == "Cat Cat Cat"
+        message = "　　　　　 ∧,,∧<br>
+        　　　　　(,,・∀・)<br>
+        　　　　～(_ｕ,ｕ<br>"
+    end
+
     s.write_time = time
-    s.message = params[:message]
+    s.message = message
     s.save
     redirect '/'
 end
@@ -45,4 +70,3 @@ delete '/del' do
     s.destroy
     redirect '/'
 end
-
